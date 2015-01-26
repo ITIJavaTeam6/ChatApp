201c89daec5121c554994a764ae49b8ecc869490
sharno <sharnoby3@gmail.com>
2015-01-26 17:53:42 +0200

    changed name of whole project

---
+++ Chat App/src/chat/database/beans/User.java
@@ -0,0 +1,97 @@
+package chat.database.beans;
+
+import java.sql.*;
+
+public class User {
+
+    private long iduser;
+
+    public long getIduser() {
+        return iduser;
+    }
+
+    public void setIduser(long iduser) {
+        this.iduser = iduser;
+    }
+
+    private String fname;
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    private String lname;
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private Timestamp bdate;
+
+    public Timestamp getBdate() {
+        return bdate;
+    }
+
+    public void setBdate(Timestamp bdate) {
+        this.bdate = bdate;
+    }
+
+    private long status;
+
+    public long getStatus() {
+        return status;
+    }
+
+    public void setStatus(long status) {
+        this.status = status;
+    }
+
+    private long gender;
+
+    public long getGender() {
+        return gender;
+    }
+
+    public void setGender(long gender) {
+        this.gender = gender;
+    }
+
+    private Timestamp cdate;
+
+    public Timestamp getCdate() {
+        return cdate;
+    }
+
+    public void setCdate(Timestamp cdate) {
+        this.cdate = cdate;
+    }
+
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
